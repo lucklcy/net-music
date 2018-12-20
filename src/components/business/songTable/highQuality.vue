52399092d627657b42ee82b04e1bb6b0a39e549a
刘丹 <liudan@ppdai.com>
2018-12-20 15:29:49 +0800

    精品歌单筛选

--- src/components/business/songTable/highQuality.vue
+++ src/components/business/songTable/highQuality.vue
@@ -2,11 +2,22 @@
   <div class="song-table-high-quality">
     <TopBar back-route-name='r_song_table_index' title='精品歌单'></TopBar>
     <div class="oper-choose border-bottom-1px">
-      <span>全部</span>
-      <span>
+      <span @click='showTableCat()'>全部</span>
+      <span @click='showTableCat()'>
         <SvgIcon :iconClass="'filtrate'" :className="'filtrate'"></SvgIcon>
         筛选
       </span>
+    </div>
+    <div class="cat-container" v-show="isShowTableCat">
+      <div class="all">全部</div>
+      <div class="cat-list">
+        <div class="cat-item" v-for='(item,index) in tableCatArray'>
+          <span :class="{'active':tableCat===item.value}" @click="changeCat(item.value)">
+            {{item.value}}
+            <SvgIcon v-if='tableCat===item.value' :iconClass="'right-triangle'" :className="'right-triangle'"></SvgIcon>
+          </span>
+        </div>
+      </div>
     </div>
     <Scroll class="container" ref="highQualitySongList" :data-list="highQualitySongListArray"
       :pullup="true" @scrollToEnd="doPullup">
@@ -47,14 +58,40 @@ import { ICreator, IPlayList } from '@/common/interface/base.ts'
 import TopBar from '~/foundation/com/topBar.vue'
 import Footer from '~/foundation/com/footer.vue'
 import Scroll from '~/foundation/base/scroll.vue'
+import { Mutation, State } from 'vuex-class'
 
 @Component({
   components: { TopBar, Footer, Scroll }
 })
 export default class SongHighQualityTable extends mixins(CommonMixin) {
+  @State tableCat: string
+  @Mutation changeTableCat: (payload: { type: number; cat: string }) => void
   private highQualitySongListArray: IPlayList[] = []
   private limit: number = 10
   private updateTime: number = 0
+  private tableCatArray: any[] = [
+    { key: 1, value: '华语' },
+    { key: 2, value: '欧美' },
+    { key: 3, value: '韩语' },
+    { key: 4, value: '日语' },
+    { key: 5, value: '粤语' },
+    { key: 6, value: '小语种' },
+    { key: 7, value: '运动' },
+    { key: 4, value: 'ACG' },
+    { key: 5, value: '影视原声' },
+    { key: 6, value: '流行' },
+    { key: 7, value: '摇滚' },
+    { key: 6, value: '后摇' },
+    { key: 7, value: '古风' },
+    { key: 4, value: '民谣' },
+    { key: 5, value: '轻音乐' },
+    { key: 6, value: '电子' },
+    { key: 7, value: '器乐' },
+    { key: 5, value: '说唱' },
+    { key: 6, value: '古典' },
+    { key: 7, value: '爵士' }
+  ]
+  private isShowTableCat: boolean = false
   private gotoDetail(id: string) {
     this.$router.push({ name: 'r_song_list', query: { id } })
   }
@@ -63,15 +100,7 @@ export default class SongHighQualityTable extends mixins(CommonMixin) {
   }
 
   private getHighQualityList() {
-    this.service
-      .getHighQualityList({ limit: this.limit })
-      .then((highQualityListResult: { playlists: IPlayList[] }) => {
-        let tempHighQualitySongListArray =
-          highQualityListResult['playlists'] && highQualityListResult['playlists']
-        this.highQualitySongListArray = tempHighQualitySongListArray
-        this.updateTime =
-          tempHighQualitySongListArray[tempHighQualitySongListArray.length - 1]['updateTime']
-      })
+    this.getPlayList({ limit: this.limit })
   }
 
   private addHighQualityList() {
@@ -88,6 +117,27 @@ export default class SongHighQualityTable extends mixins(CommonMixin) {
         )
       })
   }
+  private showTableCat() {
+    this.isShowTableCat = true
+  }
+  private changeCat(name: string) {
+    // debugger
+    this.changeTableCat({ type: 0, cat: name })
+    this.isShowTableCat = false
+    this.getPlayList({ limit: this.limit, cat: name })
+  }
+  // 获取精选歌单
+  private getPlayList(item: object) {
+    this.service
+      .getHighQualityList(item)
+      .then((highQualityListResult: { playlists: IPlayList[] }) => {
+        let tempHighQualitySongListArray =
+          highQualityListResult['playlists'] && highQualityListResult['playlists']
+        this.highQualitySongListArray = tempHighQualitySongListArray
+        this.updateTime =
+          tempHighQualitySongListArray[tempHighQualitySongListArray.length - 1]['updateTime']
+      })
+  }
 
   created() {
     this.getHighQualityList()
@@ -96,6 +146,7 @@ export default class SongHighQualityTable extends mixins(CommonMixin) {
 </script>
 <style lang="scss">
 $baseAsset: '../../../assets';
+$category-border-color: #efefef;
 .song-table-high-quality {
   @include setSize(100%, 100%);
   @include setFlexPos(column, flex-start, flex-start);
@@ -110,6 +161,59 @@ $baseAsset: '../../../assets';
     font-size: 0.39rem;
     color: #555;
   }
+  .cat-container {
+    @include setSize(100%, 50%);
+    background-color: #f5f5f5;
+    animation: fadeIn 2s 1 0s;
+    @keyframes fadeIn {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+    // margin: 10px;
+    .all {
+      margin-top: 20px;
+      @include setSize(100%, 160px);
+      text-align: center;
+      font-size: 0.44rem;
+      line-height: 160px;
+      color: #222;
+      @include border-set(all, $category-border-color);
+    }
+    .cat-list {
+      @include setFlexPos(row, flex-start, flex-start);
+      @include border-set(all, $category-border-color);
+      flex-wrap: wrap;
+      margin-top: 20px;
+      .cat-item {
+        @include setSize(25vw, 120px);
+        @include border-set(bottom, $category-border-color);
+        span {
+          @include setSize(100%, 100%);
+          display: inline-block;
+          text-align: center;
+          line-height: 120px;
+          font-size: 0.38rem;
+          padding: 10px;
+          @include border-set(right, $category-border-color);
+          &.active {
+            color: $color-highlight-background;
+            .right-triangle {
+              position: absolute;
+              bottom: 1px;
+              right: 1px;
+              font-size: 0.56rem;
+              color: $color-highlight-background;
+            }
+          }
+        }
+      }
+    }
+  }
+
   .container {
     flex: 1;
     width: 100%;
